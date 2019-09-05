a08b51dabe951d14bfd6c742a04df81a46c544a2
MG <emerenmg@gmail.com>
2019-09-05 22:13:14 +0200

    Updates

---
+++ precache-manifest.307420fb2849a2712c3311ee22d7f13f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea225524cddefbd40bb3934ce81a70e5",
+    "url": "/currency-calculator/index.html"
+  },
+  {
+    "revision": "8b0fe0369f48139ab9f2",
+    "url": "/currency-calculator/static/css/main.8ab7a477.chunk.css"
+  },
+  {
+    "revision": "ba7382990e8b23ad2eb2",
+    "url": "/currency-calculator/static/js/2.f517b95b.chunk.js"
+  },
+  {
+    "revision": "8b0fe0369f48139ab9f2",
+    "url": "/currency-calculator/static/js/main.6a856027.chunk.js"
+  },
+  {
+    "revision": "778795ff38cb6f712d0b",
+    "url": "/currency-calculator/static/js/runtime~main.a8f5e66f.js"
+  }
+]);
