769b7ffcac3a0d87560a9fc10d1880244778a8b7
MG <emerenmg@gmail.com>
2019-11-02 20:45:40 +0100

    Update Chart component

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -4,103 +4,96 @@ import { Line } from 'react-chartjs-2'
 class Exchange extends Component {
     constructor(props){
         super(props);
-        const { ct, cf } = this.props
-        const { rates } = this.props.data
-        const dates = rates.map(key => key.date);
-        const currency = rates.map(key => Object.keys(key)[0] === ct ? Object.values(key)[0] : Object.values(key)[1]);
+        const { ct, cf } = this.props;
+               
         const x = 5;
         const y = 0;
-        const labels = dates.length >= 20 && [...dates].reverse().slice(0,20);
-        const data = currency.length >= 20 && [...currency].reverse().slice(0,20);
-         
+        const { labels, label, data } = this.prepareData(this.props);
+
         this.state = {
-            cf: cf,
-            ct: ct,
-            chartLabels: labels,
-            chartData: data,
             x: x,
             y: y,
-            data: {
-                labels: labels.slice(0, x).reverse(),
-                datasets: [{
-                    label: ct,
-                    fill: false,
-                    backgroundColor: 'rgb(219,84,97)',
-                    borderColor: 'rgb(241,237,238)',
-                    pointHoverRadius: 7,
-                    data: data.slice(0, x).reverse()
-                }],
-                fill: false
-            }
+            currencyFrom: cf,
+            currencyTo: ct,
+            chartLabels: labels,
+            chartData: data,
+            data: this.chartObject(labels, label, data, {x, y})
         }
     }
 
     componentDidUpdate(prevProps) {
         if (this.props.data.rates !== prevProps.data.rates) {
-            const { x, y } = this.state
-            const { cf, ct} = this.props
-            const { rates } = this.props.data
-            const dates = rates.map(key => key.date);
-            const currency = rates.map(key => Object.keys(key)[0] === ct ? Object.values(key)[0] : Object.values(key)[1]);
-            const label = ct;
-            const labels = dates.length >= 20 && [...dates].reverse().slice(0,20);
-            const data = currency.length >= 20 && [...currency].reverse().slice(0,20);
+            const { ct, cf } = this.props;
+            const { x, y } = this.state;
+            const { labels, label, data } = this.prepareData(this.props);
+
             this.setState({
-                cf: cf,
-                ct: ct,      
+                currencyFrom: cf,
+                currencyTo: ct,
                 chartLabels: labels,
                 chartData: data,
-                data: {
-                    labels: labels.slice(y, x).reverse(),
-                    datasets: [{
-                        label: label,
-                        fill: false,
-                        backgroundColor: 'rgb(219,84,97)',
-                        borderColor: 'rgb(241,237,238)',
-                        pointHoverRadius: 7,
-                        data: data.slice(y, x).reverse()
-                    }],
-                    fill: false
-                }
+                data: this.chartObject(labels, label, data, {x, y})
             })
         }
-      }
+    }
 
+    prepareData = (props) => {
+        const { ct } = props;
+        const { rates } = props.data;
+
+        const dates = rates.map(key => key.date);
+        const currency = rates.map(key => Object.keys(key)[0] === ct ? Object.values(key)[0] : Object.values(key)[1]);
+        const label = ct;
+        const labels = dates.length >= 20 && dates.reverse().slice(0,20);
+        const data = currency.length >= 20 && currency.reverse().slice(0,20);
+        
+        return {
+            labels,
+            label,
+            data
+        }
+    }
+
+    chartObject = (labels, label, data, {x, y}) => {
+        return {
+            labels: labels.slice(y, x).reverse(),
+            datasets: [{
+                label: label,
+                fill: false,
+                backgroundColor: 'rgb(219,84,97)',
+                borderColor: 'rgb(241,237,238)',
+                pointHoverRadius: 7,
+                data: data.slice(y, x).reverse()
+            }],
+            fill: false
+        }
+    }
+    
     // display chart data
     handleData = (event) => {
-        const { chartLabels, chartData, ct, x } = this.state;
+        const { chartLabels, chartData, currencyTo, x } = this.state;
 
         const name = event.currentTarget.className;
-
         const r = name.includes('prevData') ? x + 5 : x - 5;
 
         if(r <= chartLabels.length && r > 0){
             const rx = name.includes('prevData') ? r : r;
             const ry = name.includes('prevData') ? x : r - 5;
 
-            const label = ct;
+            const label = currencyTo;
             const labels = chartLabels.slice(ry, rx).reverse();
             const data = chartData.slice(ry, rx).reverse();
+
             this.setState({
                 x: rx,
                 y: ry,
-                data: {
-                    labels: labels,
-                    datasets: [{
-                        label: label,
-                        fill: false,
-                        backgroundColor: 'rgb(219,84,97)',
-                        borderColor: 'rgb(241,237,238)',
-                        data: data
-                    }],
-                    fill: false
-                }
+                data: this.chartObject(labels, label, data, {rx, ry})
             })
         }
     }
 
     render() {
-        const { ct, data } = this.state
+        const { currencyTo, data } = this.state;
         return (
             <div className="chart" ref="chart" data-testid="chart">
                 <Line 
@@ -126,7 +119,7 @@ class Exchange extends Component {
                             yAxes: [{
                                 ticks: {
                                     callback: function(value, index, values) {
-                                        return value.toFixed(5) + ` ${ct}`;
+                                        return value.toFixed(5) + ` ${currencyTo}`;
                                     },
                                     fontColor: '#8AA29E'
                                 }
